dfc5a0b6e82c9db279e90e607d3cdefebaeec93c
ewatryscien <ewa.tryscien@gmail.com>
2022-06-02 18:35:02 +0100

    adding delete action

--- src/store/middleware/localstorage.js
+++ src/store/middleware/localstorage.js
@@ -3,27 +3,37 @@ import { v1 as uuid } from "uuid";
 
 const storage = ({ dispatch, getState }) => (next) => (action) => {
   if (action.type !== actions.tasksStorage.type) return next(action);
+
   if (action.payload.command === "store") {
     const task = {
       id: uuid(),
       description: action.payload.data,
     };
-    // todo: and add to the local storage
-    const savedTasks = loadFromLocalStorage();
-    savedTasks[task.id] = task;
-    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
-
+    //add to the local storage
+    const tasks = loadFromLocalStorage();
+    tasks[task.id] = task;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     dispatch({ type: action.payload.onSuccess, payload: task });
   }
+
   if (action.payload.command === "load") {
     const tasks = loadFromLocalStorage();
     dispatch({ type: action.payload.onSuccess, payload: tasks });
   }
+
+  if (action.payload.command === "delete") {
+    const tasks = loadFromLocalStorage();
+    console.log("tasks.id:", action.payload.data);
+    delete tasks[action.payload.data];
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    dispatch({ type: action.payload.onSuccess, payload: tasks });
+  }
+
   next(action);
 };
 
 function loadFromLocalStorage() {
-  const unparsedTasks = localStorage.getItem("savedTasks");
+  const unparsedTasks = localStorage.getItem("tasks");
   if (!unparsedTasks) {
     return {};
   }
