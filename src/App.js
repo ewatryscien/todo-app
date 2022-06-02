e0f8b9ee4de2fdec7e15f72aab21bdaaf822b78f
ewatryscien <ewa.tryscien@gmail.com>
2022-06-02 01:14:47 +0100

    main component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import React from "react";
+import { Provider } from "react-redux";
+import TasksList from "./components/TasksList";
+import configureStore from "./store/configureStore";
+import { taskAdded, taskCompleted } from "./store/tasks";
+import AddTask from "./components/AddTask";
+import { VStack, Heading } from "@chakra-ui/react";
+
+const store = configureStore();
+
+store.subscribe(() => {
+  //console.log("Store changeeeed!");
+});
+
+//const unfinishedTasks = getUnfinishedTasks(store.getState());
+
 function App() {
-  return <div></div>;
+  return (
+    <VStack p={4}>
+      <Heading mb="8" fontWeight="bold" size="2xl">
+        TODO APP
+      </Heading>
+      <Provider store={store}>
+        <div className="container">
+          <TasksList />
+          <AddTask />
+        </div>
+      </Provider>
+    </VStack>
+  );
 }
 
 export default App;
