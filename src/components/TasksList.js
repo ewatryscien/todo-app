fd48848e668a0fa201aac1a835468cd56ceeae10
ewatryscien <ewa.tryscien@gmail.com>
2022-06-02 01:50:10 +0100

    adding chakra styles

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadTasks, getAllTasks } from "../store/tasks";
+import {
+  VStack,
+  HStack,
+  Text,
+  StackDivider,
+  Button,
+  Spacer,
+  Checkbox,
+} from "@chakra-ui/react";
 
 const TasksList = () => {
   const dispatch = useDispatch();
@@ -11,13 +20,25 @@ const TasksList = () => {
   const tasks = useSelector(getAllTasks);
 
   return (
-    <div>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>Nazwa: {task.description}</li>
-        ))}
-      </ul>
-    </div>
+    <VStack
+      divider={<StackDivider />}
+      borderColor="gray.100"
+      borderWidth="2px"
+      p="4"
+      borderRadius="lg"
+      w="100%"
+      maxW={{ base: "90vh", sm: "80vh", lg: "50vh" }}
+      alignItems="stretch"
+    >
+      {tasks.map((task) => (
+        <HStack key={task.id}>
+          <Checkbox mr="4"></Checkbox>
+          <Text>{task.description}</Text>
+          <Spacer />
+          <Button>Usuń</Button>
+        </HStack>
+      ))}
+    </VStack>
   );
 };
 
