7cde5a0fa2c03134649a7d1c37d54248172f0670
ewatryscien <ewa.tryscien@gmail.com>
2022-06-03 15:08:39 +0100

    error page styling

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { VStack, Text, Heading } from "@chakra-ui/react";
 
 const Error404 = () => {
   let location = useLocation();
   return (
-    <div>
-      <h3>
-        No match for <code>{location.pathname}</code>
-      </h3>
-    </div>
+    <VStack m="20">
+      <Heading color="teal.600" fontWeight="bold" size="xl">
+        404
+      </Heading>
+      <Heading color="teal.900" fontWeight="bold" size="lg">
+        Page not found
+      </Heading>
+      <Text as="kbd" color="teal.900">
+        No match for {location.pathname}
+      </Text>
+    </VStack>
   );
 };
 
