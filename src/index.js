30ed2f803864b9b2420c83b849f456223e965009
ewatryscien <ewa.tryscien@gmail.com>
2022-06-03 14:58:21 +0100

    adding routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, VStack } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 import "./index.css";
 import App from "./App";
+import SingleTask from "./components/SingleTask";
+import Error404 from "./components/Error404";
+
+const store = configureStore();
+
+store.subscribe(() => {
+  //console.log("Store changeeeed!");
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
-    <App />
+    <Provider store={store}>
+      {" "}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/task/:id" element={<SingleTask />} />
+          <Route path="*" element={<Error404 />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </ChakraProvider>
 );
