4a828a015bfcf39a18bedba9157a895c21f6e68a
ewatryscien <ewa.tryscien@gmail.com>
2022-06-03 14:57:57 +0100

    deleting unused code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
-import { Provider } from "react-redux";
 import TasksList from "./components/TasksList";
-import configureStore from "./store/configureStore";
-import { taskAdded, taskCompleted } from "./store/tasks";
 import AddTask from "./components/AddTask";
 import { VStack, Heading } from "@chakra-ui/react";
-
-const store = configureStore();
-
-store.subscribe(() => {
-  //console.log("Store changeeeed!");
-});
-
-//const unfinishedTasks = getUnfinishedTasks(store.getState());
 
 function App() {
   return (
     <VStack p={4}>
-      <Heading mb="8" fontWeight="bold" size="2xl" color="pink.700">
+      <Heading mb="8" fontWeight="bold" size="2xl" color="teal.400">
         TODO APP
       </Heading>
-      <Provider store={store}>
-        <div className="container">
-          <TasksList />
-          <AddTask />
-        </div>
-      </Provider>
+      <TasksList />
+      <AddTask />
     </VStack>
   );
 }
